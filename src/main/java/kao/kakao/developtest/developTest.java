c26d1edd36765fa961bacc7f4b1d40b6f8759b1b
trsnathon <https://github.com/trsnathon/iTshirt.git>
2023-06-08 21:37:25 +0900

    developtest commit

---
+++ src/main/java/kao/kakao/developtest/developTest.java
@@ -0,0 +1,15 @@
+package kao.kakao.developtest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class developTest {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
